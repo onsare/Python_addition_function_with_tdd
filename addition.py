a2856578ec33a5f9498265c1b78e2b9bcd25a070
denis-onsare <denisonsare@gmail.com>
2017-04-04 08:26:16 +0300

    removed irrelevant functions

--- addition.py
+++ addition.py
@@ -14,20 +14,6 @@ class TestAdditions(unittest.TestCase):
 	def test_negative_one_plus_negative_one(self):
 		self.assertEqual(calculateAdditions("-1 + -1"),-2)
 
-def calculateSubtractions(expression):
-	numbers = expression.split('-')
-	total = 0
-	for number in numbers:
-		total -= int(number)
-	return total
-
-class TestAdditions(unittest.TestCase):
-	def test_one_minus_one(self):
-		self.assertEqual(calculateSubtractions("1-1"),0)
-	def test_negative_one_minus_negative_one(self):
-		self.assertEqual(calculateSubtractions("-1 - 1"),-2)
-	
-
 
 
 
@@ -37,5 +23,3 @@ class TestAdditions(unittest.TestCase):
 if __name__ == '__main__':
 	unittest.main()
 
-# formula for getting interest rate A = P(1 + rt)
-# r is the decimal form of R/100; r and t are in the same units of time
